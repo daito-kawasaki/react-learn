c6cf399e253974af6a13ead322585b4f0e011479
daito-kawasaki <daito.wb.b@gmail.com>
2024-06-19 16:41:57 +0900

    todoリストの追加機能実装

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
   const [todos, setTodos] = useState([{ id: 1, name: "Todo1", completed: false }]);
   
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false }];
+    });
+
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos)
+  };
+
   return ( 
   <>
-    <TodoList todos = {todos} />
-    <input type="text" />
-    <button>タスクを追加</button>
+    <TodoList todos = {todos} toggleTodo={toggleTodo}/>
+    <input type="text" ref={todoNameRef}/>
+    <button onClick={handleAddTodo}>タスクを追加</button>
     <button>完了したタスクの削除</button>
     <div>残りのタスク:0</div>
   </>
